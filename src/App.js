ac70eb862772bea0e553ec9845ddc0a16ffb7ac7
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2025-09-19 12:26:31 -0400

    established background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from "react";
 import WelcomePage from "./WelcomePage";
 
-
 function App() {
   return (
     <div className="App">
---
+++ src/BackgroundDesign.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./BackgroundDesign.css";
+
+const BackgroundDesign = () => {
+
+    return (
+        <div class="background">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
+export default BackgroundDesign;
