8284d7b49d377eae2b0ef380e032af878dfef0f4
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2025-09-19 13:33:06 -0400

    updated links and details

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./WelcomePage.css";
+import lovers from "./imgs/lovers.jpeg";
 
 const WelcomePage = () => {
 
     return (
         <div className="WelcomePage d-flex">
             <div className="WelcomeHeader">
-                <h1 className="typed-out">✨WELCOME✨</h1>
+                <h1 className="typed-out">Oh, Hi There 🫶</h1>
             </div>
-            <div className="EtsyButton">
-                <a href="https://littlehaunteddesigns.etsy.com" className="PortfolioLink" title="https://littlehaunteddesigns.etsy.com" target="_blank" rel="noreferrer">ETSY</a>
+
+            <div className="SpicyButton">
+                <a href="https://onlyfans.com/baebeeangel" className="PortfolioLink" title="https://onlyfans.com/baebeeangel" target="_blank" rel="noreferrer">Spicy Site</a>
+            </div>
+            <div className="RedditButton">
+                <a href="https://www.reddit.com/user/baebeeangel/" className="PortfolioLink" title="https://www.reddit.com/user/baebeeangel/" target="_blank" rel="noreferrer">Reddit</a>
             </div>
             <div className="InstagramButton">
-                <a href="https://www.instagram.com/haunted_design/" className="PortfolioLink" title="https://www.instagram.com/haunted_design/" target="_blank" rel="noreferrer">INSTAGRAM</a>
+                <a href="https://www.instagram.com/baebee.angel" className="PortfolioLink" title="https://www.instagram.com/baebee.angel" target="_blank" rel="noreferrer">Instagram</a>
             </div>
+            <div className="lovers"><img src={lovers} alt="statue people kissing"></img></div>
         </div>
     )
 }
